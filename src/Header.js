ddd65f1fb36d8c8725d8e37c3464c80f6b49fff2
Benedict-Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-13 13:19:41 +0100

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <div>
+            <header>
+                <img src="https://images.app.goo.gl/X7U74u9mXw84R5BBA" alt="" />
+                <p>Meme Generator</p>
+            </header>
+        </div>
+    )
+}
+
+export default Header
